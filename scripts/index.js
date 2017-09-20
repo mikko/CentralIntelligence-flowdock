242f1821203cd1bb6b67a611b2535e3d415ce265
Mikko Pohja <mikko.pohja@futurice.com>
2017-09-20 14:39:52 +0300

    Command support and user group sync minimal implementation

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,49 @@
+require('dotenv').config();
+const _ = require('lodash');
 const CIClient = require('ci-client');
 const CIconfig = require('../client-config.js');
-
-require('dotenv').config();
-
 const client = new CIClient(CIconfig);
+
+const commandPrefix = '.';
 
 const userParser = ctx => ctx.envelope.user.id;
 
 client.setUserParser(userParser);
-
 client.setPrivateChatParser(ctx => ctx.envelope.room === undefined);
 
-client.setUserPropertiesParser(ctx => {
-  return {
-    flowdockUsername: ctx.envelope.user.name,
-    flowdockId: ctx.envelope.user.id
-  };
-});
+client.setUserPropertiesParser(ctx => ({
+  flowdockUsername: ctx.envelope.user.name,
+  flowdockId: ctx.envelope.user.id
+}));
+
+const updateGroupUsers = (robot) => {
+  robot.adapter.flows.forEach(flow => {
+    const users = flow.users.map(user => user.id);
+    const flowName = flow.parameterized_name;
+    client.sendCommand('setGroupUsers', { group: flowName, type: 'flowdock', users: users });
+  });
+};
 
 function Client(robot) {
-  const messageReceiver = (action, message, context) => {
-    console.dir(context.envelope);
-    robot.send(context.envelope, message);
-  };
+  // Stupid way to sync just in start and with a timeout
+  setTimeout(() => {
+    updateGroupUsers(robot);
+  }, 5000);
 
-  client.setReceiver(messageReceiver);
+  client.setReceiver((action, message, context) => robot.send(context.envelope, message));
 
   robot.respond(/(.*)/i, (res) => {
-    require('fs').writeFileSync('envelope.json', JSON.stringify(res.envelope, null, 2));
-    const question = res.match[1];
-    client.sendMessage(question, { envelope: res.envelope });
+    const message = res.match[1];
+    const context = { envelope: res.envelope };
 
+    if (message[0] === commandPrefix) {
+      const msgTokens = message.split(' ');
+      const command = msgTokens.shift().split(commandPrefix).pop();
+      const params = msgTokens.join(" ");
+      client.sendCommand(command, params, context);
+    } else {
+      client.sendMessage(message, context);
+    }
   });
 }
 
